7d77f80795b98b61a8dd93bd5f768b32013fe7e1
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-12-17 22:20:39 +0200

    db setup: DatabaseConfig class created

---
+++ src/main/java/wad/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad.config;
+
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ *
+ * @author mikko
+ */
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
