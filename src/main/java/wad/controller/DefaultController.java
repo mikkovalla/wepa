933935ac3cb1225aa754bdc5691ca8aa0edca620
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-12-18 04:22:17 +0200

    updating db connection

--- src/main/java/wad/controller/DefaultController.java
+++ src/main/java/wad/controller/DefaultController.java
@@ -6,25 +6,26 @@
 package wad.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import wad.repository.JobRepository;
+import wad.service.JobService;
 
 /**
  *
  * @author mikko
  */
 @Controller
+@EnableJpaRepositories
 public class DefaultController {
 
     @Autowired
-    private JobRepository jobRepo;
+    private JobService jobRepo;
 
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("jobs", jobRepo.findAll());
+        model.addAttribute("jobs", jobRepo.all());
         return "index";
     }
-
 }
