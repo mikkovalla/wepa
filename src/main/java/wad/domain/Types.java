f2ec40010248015637daa46a92022f0f10f91794
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-12-19 01:04:14 +0200

    updating

--- src/main/java/wad/domain/Types.java
+++ src/main/java/wad/domain/Types.java
@@ -9,9 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import org.springframework.data.jpa.domain.AbstractPersistable;
@@ -21,7 +18,6 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
  * @author mikko
  */
 @Entity
-
 public class Types extends AbstractPersistable<Long>{
 
     @NotNull
