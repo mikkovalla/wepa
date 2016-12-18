2d07fb6797b95b4ec12b83d25ec88e098850da43
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-12-18 15:09:02 +0200

    getting heroku db to work

--- src/main/java/wad/config/DatabaseConfig.java
+++ src/main/java/wad/config/DatabaseConfig.java
@@ -7,7 +7,8 @@ package wad.config;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import org.apache.commons.dbcp2.BasicDataSource;
+import javax.sql.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -19,20 +20,27 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("production")
 public class DatabaseConfig {
-    
+
+    private static final String HEROKU_DATABASE_URL_KEY = "DATABASE_URL";
+    private static final String HEROKU_POSTGRE_URL_PREFIX = "jdbc:postgresql://";
+    private static final String POSTGRESQL_DRIVER_CLASS = "org.postgresql.Driver";
+
     @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv(HEROKU_DATABASE_URL_KEY));
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        String dbUrl = HEROKU_POSTGRE_URL_PREFIX + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+        PoolProperties pp = new PoolProperties();
+        pp.setDriverClassName(POSTGRESQL_DRIVER_CLASS);
+        pp.setInitialSize(1);
+        pp.setPassword(password);
+        pp.setUsername(username);
+        pp.setUrl(dbUrl);
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource(pp);
 
-        return basicDataSource;
+        return dataSource;
     }
 }
