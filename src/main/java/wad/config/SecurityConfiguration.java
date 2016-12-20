f808b1a52e2c35c3e15e24e3e9449438b37f0b78
Miksu <mikko.vallaskivi@gmail.com>
2016-12-20 06:02:01 +0200

    security config

---
+++ src/main/java/wad/config/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import wad.service.CustomEmployeeDetailsService;
+import wad.service.CustomEmployerDetailsService;
+
+/**
+ *
+ * @author Mikko
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomEmployeeDetailsService employeeDetailsService;
+
+    @Autowired
+    private CustomEmployerDetailsService employerDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+        http.formLogin()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(employeeDetailsService).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(employerDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/wad/controller/EmployerController.java
+++ src/main/java/wad/controller/EmployerController.java
@@ -5,10 +5,20 @@
  */
 package wad.controller;
 
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import wad.domain.Employer;
 import wad.service.EmployerService;
 
 /**
@@ -22,8 +32,36 @@ public class EmployerController {
     private EmployerService employerService;
 
     @RequestMapping(value = "/register/employer", method = RequestMethod.POST)
-    public String employerRegister() {
+    public String employerRegister(@Valid @ModelAttribute("employer") Employer employer, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        if (employerService.findByUsername(employer.getEmail()) != null) {
+            bindingResult.rejectValue("username", "error.employer", "Käyttäjä tunnus on jo olemassa!");
+        } else if (employerService.findByEmail(employer.getEmail()) != null) {
+            bindingResult.rejectValue("email", "error.employer", "Email osoite on jo rekisteröity palveluun!");
+        }
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        try {
+            Employer emp = new Employer();
+            emp.setCompanyName(employer.getCompanyName());
+            emp.setEmail(employer.getEmail());
+            emp.setUsername(employer.getUsername());
+            emp.setPassword(employer.getPassword());
+            emp.setCompanyDescription(employer.getCompanyDescription());
+            emp.setCreated(new Date());
+            employerService.addEmployer(emp);
 
-        return "";
+            redirectAttributes.addFlashAttribute("message", "Tervetuloa palveluun " + emp.getCompanyName() + " Voit nyt kirjautua sisään!");
+            return "redirect:/login";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "register";
+        }
+    }
+
+    @RequestMapping(value = "employer/logout", method = RequestMethod.POST)
+    public String getLogout(HttpSession httpSession, Model model) throws ServletException {
+        SecurityContextHolder.clearContext();
+        return "index";
     }
 }
