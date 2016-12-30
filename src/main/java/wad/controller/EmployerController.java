0bb0d144c0aab0eee6e76057e97cf1f8a015c7f0
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-12-30 15:32:28 +0200

    login ja register toimii

--- src/main/java/wad/controller/EmployerController.java
+++ src/main/java/wad/controller/EmployerController.java
@@ -29,85 +29,76 @@ import wad.service.JobService;
  */
 @Controller
 public class EmployerController {
-
+    
     @Autowired
     private EmployerService employerService;
-
+    
     @Autowired
     private JobService jobService;
-
+    
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String employerRegister(@Valid @ModelAttribute("employer") Employer employer, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-
-        if (employerService.findByUsername(employer.getUsername()) != null) {
-            bindingResult.rejectValue("username", "error.employer", "Käyttäjä tunnus on jo olemassa!");
-        } else if (employerService.findByEmail(employer.getEmail()) != null) {
-            bindingResult.rejectValue("email", "error.employer", "Email osoite on jo rekisteröity palveluun!");
-        }
+    public String employerRegister(HttpSession session, @Valid @ModelAttribute("employer") Employer employer, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "register";
         }
         try {
-            Employer emp = new Employer();
-            emp.setCompanyName(employer.getCompanyName());
-            emp.setEmail(employer.getEmail());
-            emp.setUsername(employer.getUsername());
-            emp.setPassword(employer.getPassword());
-            emp.setCompanyDescription(employer.getCompanyDescription());
-            emp.setCreated(new Date());
+            Employer emp = employer;
             employerService.addEmployer(emp);
-
-            redirectAttributes.addFlashAttribute("message", "Tervetuloa palveluun " + emp.getCompanyName() + " Voit nyt kirjautua sisään!");
+            session.setAttribute("employer", emp);
             return "employer";
+            
         } catch (Exception e) {
             model.addAttribute("error", e.getMessage());
             return "register";
         }
     }
-
+    
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public String loginEmployer(RedirectAttributes redirect, @RequestParam String username, @RequestParam String password) {
+    public String loginEmployer(HttpSession session, RedirectAttributes redirect, @RequestParam String username, @RequestParam String password) {
         if (employerService.findByUsername(username) == null) {
             redirect.addAttribute("message", "Väärä käyttäjätunnus");
             return "redirect:/login";
         }
-
+        Employer employer = employerService.findByUsername(username);
+        session.setAttribute("employer", employer);
         return "employer";
     }
-
+    
     @RequestMapping(value = "/employer/logout", method = RequestMethod.POST)
-    public String getLogout() {
+    public String getLogout(HttpSession session) {
         SecurityContextHolder.clearContext();
+        session.invalidate();
         return "index";
     }
-
+    
     @RequestMapping(value = "/employer/{id}/update", method = RequestMethod.POST)
-    public String employerUpdate(@Valid @ModelAttribute("employer") Employer employer,
+    public String employerUpdate(HttpSession session, @Valid @ModelAttribute("employer") Employer employer,
             BindingResult bindingResult,
             RedirectAttributes redirectAttributes,
             Model model,
             @PathVariable Long id) {
-
+        
         if (bindingResult.hasErrors()) {
             return "employer";
         }
         try {
-            Employer emp = employerService.findOne(id);
+            Employer emp = employerService.loggedIn();
             emp.setCompanyName(employer.getCompanyName());
             emp.setEmail(employer.getEmail());
             emp.setUsername(employer.getUsername());
             emp.setPassword(employer.getPassword());
             emp.setCompanyDescription(employer.getCompanyDescription());
             employerService.update(emp);
-
-            redirectAttributes.addAttribute("message", "Tiedot päivitetty onnistuneesti!");
+            
+            redirectAttributes.addFlashAttribute("message", "Tiedot päivitetty onnistuneesti!");
             return "redirect:/employer";
+            
         } catch (Exception e) {
             redirectAttributes.addAttribute("message", "Jotain meni pieleen" + e);
             return "redirect:/employer";
         }
     }
-
+    
     @RequestMapping(value = "/employer", method = RequestMethod.GET)
     public String employerDetailsPage(Model model, HttpSession session) {
         Employer employer = employerService.loggedIn();
@@ -115,7 +106,7 @@ public class EmployerController {
         model.addAttribute("employerJobs", jobService.allByEmployer(employerService.loggedIn().getUsername()));
         return "employer";
     }
-
+    
     @RequestMapping(value = "/employer/delete", method = RequestMethod.DELETE)
     public String deleteEmployer(RedirectAttributes redirectAttributes) {
         SecurityContextHolder.clearContext();
