4544b66ef9e9c5a4ee3d51c91c0eea33f3a615b4
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-12-30 19:37:56 +0200

    postscontruct update

--- src/main/java/wad/service/JobService.java
+++ src/main/java/wad/service/JobService.java
@@ -5,16 +5,19 @@
  */
 package wad.service;
 
+import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import wad.domain.Area;
 import wad.domain.Categories;
+import wad.domain.Employer;
 import wad.domain.Job;
 import wad.domain.Types;
 import wad.repository.AreaRepository;
 import wad.repository.CategoriesRepository;
+import wad.repository.EmployerRepository;
 import wad.repository.JobRepository;
 import wad.repository.TypesRepository;
 
@@ -34,50 +37,59 @@ public class JobService {
     @Autowired
     private TypesRepository typeRepo;
     @Autowired
-    private LoggedInEmployerService employerLoggedIn;
+    EmployerRepository employerRepo;
     @Autowired
     private AreaRepository areaRepo;
 
     @PostConstruct
     public void init() {
         // demo content
-        categoryRepo.save(new Categories("Front-end devaaja"));
-        categoryRepo.save(new Categories("Back-end devaaja"));
-        categoryRepo.save(new Categories("UI designeri"));
-        categoryRepo.save(new Categories("Fullstack guru"));
-        categoryRepo.save(new Categories("Pelin rakentaja"));
-        categoryRepo.save(new Categories("iOS / Android / Mobile osaaja"));
-        categoryRepo.save(new Categories("Wordpress / Magento / Joomla / Ecommerce häkkääjä"));
+        Categories c1 = categoryRepo.save(new Categories("Front-end devaaja"));
+        Categories c2 = categoryRepo.save(new Categories("Back-end devaaja"));
+        Categories c3 = categoryRepo.save(new Categories("UI designeri"));
+        Categories c4 = categoryRepo.save(new Categories("Fullstack guru"));
+        Categories c5 = categoryRepo.save(new Categories("Pelin rakentaja"));
+        Categories c6 = categoryRepo.save(new Categories("iOS / Android / Mobile osaaja"));
+        Categories c7 = categoryRepo.save(new Categories("Wordpress / Magento / Joomla / Ecommerce häkkääjä"));
 
-        typeRepo.save(new Types("Täys päivä", "#4c9ef1"));
-        typeRepo.save(new Types("Osa-aika", "#81b800"));
-        typeRepo.save(new Types("Freelancer", "#f4f4f4"));
+        Types t1 = typeRepo.save(new Types("Täys päivä", "#4c9ef1"));
+        Types t2 = typeRepo.save(new Types("Osa-aika", "#81b800"));
+        Types t3 = typeRepo.save(new Types("Freelancer", "#f4f4f4"));
         
-        areaRepo.save(new Area("Espoo"));
-        areaRepo.save(new Area("Helsinki"));
-        areaRepo.save(new Area("Tampere"));
-        areaRepo.save(new Area("Vantaa"));
-        areaRepo.save(new Area("Oulu"));
-        areaRepo.save(new Area("Turku"));
+        Area a1 = areaRepo.save(new Area("Espoo"));
+        Area a2 = areaRepo.save(new Area("Helsinki"));
+        Area a3 = areaRepo.save(new Area("Tampere"));
+        Area a4 = areaRepo.save(new Area("Vantaa"));
+        Area a5 = areaRepo.save(new Area("Oulu"));
+        Area a6 = areaRepo.save(new Area("Turku"));
+        
+        Employer e1 = employerRepo.save(new Employer("JavaGurut", "java@java.com", "javaguru", "javaguru", "javagurut on hieno lafka", new Date()));
+        Employer e2 = employerRepo.save(new Employer("Osaajat", "osaajat@osaajat.com", "osaajat", "osaajat", "osaajat on hieno lafka", new Date()));
+        Employer e3 = employerRepo.save(new Employer("KoodinVääntäjät", "css@css.com", "csshtml", "csshtml", "jKoodinvääntäjät on hieno lafka joka osaa", new Date()));
+    
 
-        jobRepo.save(new Job(categoryRepo.findOne(1L), employerService.findOne(1L), typeRepo.findOne(1L), areaRepo.findOne(1L),
+        Job job1 = new Job(c1, e1, t1, a1,
                 "Html ja Css taituri",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
                 + "Suspendisse dapibus risus at dapibus malesuada. "
                 + "Vestibulum euismod, velit sit amet tempor ornare, "
-                + "nulla augue mattis mi, nec ultricies dolor augue nec lectus."));
-        jobRepo.save(new Job(categoryRepo.findOne(2L), employerService.findOne(2L), typeRepo.findOne(2L), areaRepo.findOne(2L),
+                + "nulla augue mattis mi, nec ultricies dolor augue nec lectus.");
+        Job job2 = new Job(c2, e2, t2, a2,
                 "JavaMaster",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
                 + "Suspendisse dapibus risus at dapibus malesuada. "
                 + "Vestibulum euismod, velit sit amet tempor ornare, "
-                + "nulla augue mattis mi, nec ultricies dolor augue nec lectus."));
-        jobRepo.save(new Job(categoryRepo.findOne(3L), employerService.findOne(3L), typeRepo.findOne(3L), areaRepo.findOne(3L),
+                + "nulla augue mattis mi, nec ultricies dolor augue nec lectus.");
+        Job job3 = new Job(c3, e3, t3, a3,
                 "JavaScript/React/Angular jamppa",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
                 + "Suspendisse dapibus risus at dapibus malesuada. "
                 + "Vestibulum euismod, velit sit amet tempor ornare, "
-                + "nulla augue mattis mi, nec ultricies dolor augue nec lectus."));
+                + "nulla augue mattis mi, nec ultricies dolor augue nec lectus.");
+        
+        jobRepo.save(job1);
+        jobRepo.save(job2);
+        jobRepo.save(job3);
     }
 
     public List<Job> all() {
