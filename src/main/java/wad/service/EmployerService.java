da8d71f8d317c74f649ea8e4e79b7bb9f8a5a501
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-12-30 19:39:05 +0200

    post construct update

--- src/main/java/wad/service/EmployerService.java
+++ src/main/java/wad/service/EmployerService.java
@@ -5,12 +5,8 @@
  */
 package wad.service;
 
-import java.util.Date;
 import java.util.List;
-import javax.annotation.PostConstruct;
-import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import wad.domain.Employer;
 import wad.repository.EmployerRepository;
@@ -27,14 +23,6 @@ public class EmployerService {
 
     @Autowired
     private LoggedInEmployerService employerLoggedIn;
-
-    @PostConstruct
-    public void init() {
-        // demo content
-        employerRepo.save(new Employer("JavaGurut", "java@java.com", "javaguru", "javaguru", "javagurut on hieno lafka", new Date()));
-        employerRepo.save(new Employer("Osaajat", "osaajat@osaajat.com", "osaajat", "osaajat", "osaajat on hieno lafka", new Date()));
-        employerRepo.save(new Employer("KoodinVääntäjät", "css@css.com", "csshtml", "csshtml", "jKoodinvääntäjät on hieno lafka joka osaa", new Date()));
-    }
 
     public Employer findOne(Long id) {
         return employerRepo.findOne(id);
